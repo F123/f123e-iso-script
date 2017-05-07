59dce66d164b8a0902f5de4504aa1fea04a62980
Mike Ray <mike.ray@btinternet.com>
2017-05-07 18:51:14 +0000

    scripts/makecustomrepo.sh

--- scripts/makecustomrepo.sh
+++ scripts/makecustomrepo.sh
@@ -4,16 +4,11 @@ set -e
 
 ABS=./abs
 REPO="/opt/pacman/x86_64/customrepo"
-NAME=repo
+NAME=repo.db.tar.gz
 
+find ${ABS} -name *.pkg.tar.xz -exec cp {} ${REPO} \;
 
-
-#find ${ABS} -name *.pkg.tar.xz -exec cp {} ${REPO} \;
-ls "${ABS}/*.pkg.tar.xz"
-
-#repo-add /opt/pacman/x86_64/customrepo/repo.tar.gz 
-
-#repo-add /opt/customrepo/x86_64/customrepo.db.tar.gz /opt/customrepo/x86_64/*.pkg.tar.xz
+repo-add ${REPO}/${NAME} ${REPO}/*.pkg.tar.xz
 
 exit 0
 
