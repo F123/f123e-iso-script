bcd079f895940472c7bac475ef5d0490f7caa5f5
Mike Ray <mike.ray@btinternet.com>
2017-05-07 18:03:34 +0000

    On branch master Changes to be committed:
    	modified:   configs/f123e/packages.both
    	modified:   configs/f123e/pacman.conf
    	modified:   scripts/makecustomrepo.sh

--- scripts/makecustomrepo.sh
+++ scripts/makecustomrepo.sh
@@ -2,9 +2,18 @@
 
 set -e
 
-find ./abs -name *.pkg.tar.xz -exec cp {} /opt/customrepo/x86_64 \;
+ABS=./abs
+REPO="/opt/pacman/x86_64/customrepo"
+NAME=repo
 
-repo-add /opt/customrepo/x86_64/customrepo.db.tar.gz /opt/customrepo/x86_64/*.pkg.tar.xz
+
+
+#find ${ABS} -name *.pkg.tar.xz -exec cp {} ${REPO} \;
+ls "${ABS}/*.pkg.tar.xz"
+
+#repo-add /opt/pacman/x86_64/customrepo/repo.tar.gz 
+
+#repo-add /opt/customrepo/x86_64/customrepo.db.tar.gz /opt/customrepo/x86_64/*.pkg.tar.xz
 
 exit 0
 
