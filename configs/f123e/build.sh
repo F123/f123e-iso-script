55fae5437a2d9336c7712b070adc545bbca55c94
Mike Ray <mike.ray@btinternet.com>
2017-05-16 23:34:00 +0000

    Change to configs/f123e/build.sh fixing messed up merge

--- configs/f123e/build.sh
+++ configs/f123e/build.sh
@@ -4,7 +4,7 @@ set -e -u
 
 iso_name=F123e
 iso_label="ARCH_$(date +%Y%m%d)"
-iso_release=$(git describe | cut -f1 -d-)
+iso_release=
 iso_version="$(date +%Y.%m.%d)"
 install_dir=arch
 work_dir=work
