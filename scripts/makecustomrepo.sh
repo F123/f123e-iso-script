c5f9357813bb1c2f699fa666a8d5824912d68a30
Mike Ray <mike.ray@btinternet.com>
2017-09-20 03:37:52 +0100

    modified:   abs/tclx/PKGBUILD 	modified:   configs/f123e/.gitignore 	new file:   configs/f123e/airootfs/etc/dialogrc 	deleted:    configs/f123e/airootfs/opt/pacman/x86_64/f123elivecd/.gitignore 	deleted:    configs/f123e/airootfs/opt/pacman/x86_64/f123elivecd/.gitkeep 	modified:   configs/f123e/packages.x86_64 	modified:   scripts/makecustomrepo.sh

--- scripts/makecustomrepo.sh
+++ scripts/makecustomrepo.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-ABS=./abs
+ABS=../abs
 ARCH=x86_64
 REPO="/opt/pacman/${ARCH}/f123elivecd"
 NAME=f123elivecd.db.tar.gz
 
-#find ${ABS} -name *.pkg.tar.xz -exec cp {} ${REPO} \;
+find ${ABS} -name *.pkg.tar.xz -exec cp {} ${REPO} \;
 
 repo-add ${REPO}/${NAME} ${REPO}/*.pkg.tar.xz
 
