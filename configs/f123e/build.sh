4ca863ac44ee455283e0b31c9332e928519a7585
Mike Ray <mike.ray@btinternet.com>
2017-12-21 19:41:28 +0000

    On branch master
    	modified:   configs/f123e/build.sh

--- configs/f123e/build.sh
+++ configs/f123e/build.sh
@@ -5,7 +5,7 @@ set -e -u
 
 iso_name=F123e
 iso_label="ARCH_$(date +%Y%m%d)"
-iso_release=
+iso_release=$(git describe | cut -f1 -d-)
 iso_version="$(date +%Y.%m.%d)"
 install_dir=arch
 work_dir=work
